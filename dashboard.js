47731cf5f8ffd6a0fb06eb13d7768c7aeed412e4
shravya117 <139553702+shravya117@users.noreply.github.com>
2024-07-02 21:35:28 +0530

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Dashboard = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h2>Welcome to the Dashboard</h2>
+          {/* Add more components and features as needed */}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Dashboard;
